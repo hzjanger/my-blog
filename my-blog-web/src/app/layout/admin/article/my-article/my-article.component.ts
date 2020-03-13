cfafc6dde9fe8b8e1d75db3510bf001f6538d48b
hzjanger <1305696249@qq.com>
2020-03-13 16:32:24 +0800

    我的文章分页

--- my-blog-web/src/app/layout/admin/article/my-article/my-article.component.ts
+++ my-blog-web/src/app/layout/admin/article/my-article/my-article.component.ts
@@ -6,69 +6,91 @@ import {CodeEnum} from "../../../../entity/code-enum";
 import {PageResult} from "../../../../entity/page-result";
 import {MatDialog} from "@angular/material/dialog";
 import {
-  ConfirmDialogComponent,
-  ConfirmDialogInputModule
+    ConfirmDialogComponent,
+    ConfirmDialogInputModule
 } from "../../../../@theme/component/confirm-dialog/confirm-dialog.component";
 import {SnackBarService} from "../../../../service/snackBar.service";
+import {PageEvent} from "@angular/material/paginator";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
-  selector: 'app-my-article',
-  templateUrl: './my-article.component.html',
-  styleUrls: ['./my-article.component.scss']
+    selector: 'app-my-article',
+    templateUrl: './my-article.component.html',
+    styleUrls: ['./my-article.component.scss']
 })
 export class MyArticleComponent implements OnInit {
 
-  current: number = 1;
+    pageIndex: number = 1;
 
-  pageSize: number = 20;
+    pageSize: number = 20;
 
-  pageResult: PageResult<Blog>;
+    pageResult: PageResult<Blog>;
 
-  constructor(private blogService: BlogService, private dialog: MatDialog, private snackbarService: SnackBarService) {
-  }
+    constructor(private blogService: BlogService, private dialog: MatDialog, private snackbarService: SnackBarService,
+                private router: Router, private route: ActivatedRoute) {
+    }
 
-  ngOnInit() {
-    this.getMyBlogList();
-  }
-
-  getMyBlogList() {
-
-    this.blogService.findNewestBlog(this.userId, this.current, this.pageSize).subscribe(data => {
-      if (data.code === CodeEnum.SUCCESS) {
-        this.pageResult = data.data;
-      }
-    })
-  }
-
-  /**
-   * 删除博客
-   * @param blog 博客内容
-   */
-  deleteBlog(blog: Blog) {
-    const dialogData: ConfirmDialogInputModule = {title: '删除提示', content: '你确定要删除吗?'};
-    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
-      width: '520px',
-      data: dialogData
-    });
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.blogService.deleteBlog(blog.id).subscribe(data => {
-          if (data.code === CodeEnum.SUCCESS) {
-            this.snackbarService.success(data.message);
+    ngOnInit() {
+        this.route.queryParamMap.subscribe((params: Params) => {
+            this.pageIndex = +params.get('pageIndex') || 1;
+            this.pageSize = +params.get('pageSize') || 10;
             this.getMyBlogList();
-          }
+        });
+
+
+    }
+
+    getMyBlogList() {
+        this.blogService.findNewestBlog(this.userId, this.pageIndex, this.pageSize).subscribe(data => {
+            if (data.code === CodeEnum.SUCCESS) {
+                this.pageResult = data.data;
+            }
         })
+    }
 
-      }
-    })
-  }
+    /**
+     * 删除博客
+     * @param blog 博客内容
+     */
+    deleteBlog(blog: Blog) {
+        const dialogData: ConfirmDialogInputModule = {title: '删除提示', content: '你确定要删除吗?'};
+        const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+            width: '520px',
+            data: dialogData
+        });
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.blogService.deleteBlog(blog.id).subscribe(data => {
+                    if (data.code === CodeEnum.SUCCESS) {
+                        this.snackbarService.success(data.message);
+                        this.getMyBlogList();
+                    }
+                })
 
-  get userId() {
-    return JSON.parse(StorageMessage.getUserInfo()).userId;
-  }
+            }
+        })
+    }
 
-  get nickName() {
-    return JSON.parse(StorageMessage.getUserInfo()).nickName;
-  }
+    /**
+     * 分页事件
+     * @param pageEvent 分页事件
+     */
+    page(pageEvent: PageEvent) {
+        this.router.navigate(['./'], {
+            queryParams: {
+                pageIndex: pageEvent.pageIndex + 1,
+                pageSize: pageEvent.pageSize
+            },
+            relativeTo: this.route
+        })
+    }
+
+    get userId() {
+        return JSON.parse(StorageMessage.getUserInfo()).userId;
+    }
+
+    get nickName() {
+        return JSON.parse(StorageMessage.getUserInfo()).nickName;
+    }
 
 }
