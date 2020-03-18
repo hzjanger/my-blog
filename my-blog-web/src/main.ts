8a4f1306a8548140cc40a412cdd3d8098c5b5629
hzjanger <1305696249@qq.com>
2020-03-18 15:21:38 +0800

    添加首屏加载动画

--- my-blog-web/src/main.ts
+++ my-blog-web/src/main.ts
@@ -1,13 +1,22 @@
 import 'hammerjs';
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+
+const bootstrap = () => {
+  return platformBrowserDynamic().bootstrapModule(AppModule);
+};
+
+bootstrap().then(() => {
+  document.getElementById('nb-global-spinner').style.display = 'none';
+}).catch(err => console.error(err));
